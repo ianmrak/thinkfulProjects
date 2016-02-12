7f336f1df181e233e94860e6d2f38775bb340b55
Ian Mrak <ianmrak@gmail.com>
2016-02-12 01:52:45 -0600

    If Else Excercise

---
+++ ifElse.js
@@ -0,0 +1,23 @@
+var pluralizer = function(noun, number) {
+  var tempNoun = noun;
+  if (number > 1 || number === 0) {
+    switch(tempNoun.toLowerCase()) {
+      case "sheep":
+        console.log(number + " " + tempNoun);
+        break;
+      case "goose":
+        console.log(number + " " + tempNoun);
+        break;
+      case "deer":
+        console.log(number + " " + tempNoun);
+        brea;
+      default:
+        console.log(number + " " + noun + "s");
+        break;
+    }
+  } else {
+    console.log(number + " " + noun);
+  }
+};
+
+pluralizer("cat", 10);
