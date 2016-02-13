e8b430a2dd2b52c7472c3b62b3b33097ea12dace
Ian Mrak <ianmrak@gmail.com>
2016-02-13 17:43:59 -0600

    used forEach instead of for loop

--- lengthOfString.js
+++ lengthOfString.js
@@ -2,11 +2,11 @@ function sentenceBreakdown(str) {
   var wordArray = str.split(" "),
       wordLength = 0;
 
-  for (var i = 0; i < wordArray.length; i++) {
-    wordLength += (wordArray[i].length);
-  }
+  wordArray.forEach(function(word) {
+    wordLength += (word.length);
+  });
   console.log("This string has " + (wordArray.length) +
-              " words. The average length of each word is " +     (wordLength / wordArray.length) + ".");
+              " words. The average length of each word is " + (wordLength / wordArray.length) + ".");
 }
 
 sentenceBreakdown("I'm testing this sentence");
